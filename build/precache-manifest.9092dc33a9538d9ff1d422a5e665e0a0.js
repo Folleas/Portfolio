469f4e0c678b8e7403d08c1670bb404fbe5e0942
Neh_l <rayann.folleas@epitech.eu>
2020-06-07 19:55:40 +0200

    [ADD] website

---
+++ build/precache-manifest.9092dc33a9538d9ff1d422a5e665e0a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e8df34057062e9aebe09a75383b167",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "86f87116cf8a1ad1a31b",
+    "url": "/Portfolio/static/css/main.2c47463b.chunk.css"
+  },
+  {
+    "revision": "17087e4bde85322553c3",
+    "url": "/Portfolio/static/js/2.5e931307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.5e931307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f87116cf8a1ad1a31b",
+    "url": "/Portfolio/static/js/main.38d16d0a.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "9e1023954c751fb4321a5ecf917991bd",
+    "url": "/Portfolio/static/media/Nanotekspice.9e102395.gif"
+  },
+  {
+    "revision": "7712ca56986a0fa4b578252b26a8067a",
+    "url": "/Portfolio/static/media/Plazza.7712ca56.gif"
+  },
+  {
+    "revision": "58416de6a69f35db2c0aba9520b3d14d",
+    "url": "/Portfolio/static/media/background1.58416de6.jpeg"
+  }
+]);
